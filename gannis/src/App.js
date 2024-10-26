62f64ceb588cc153207955a09f905ec7b49880e1
angieVentura <angiev.d.23@gmail.com>
2024-10-26 19:07:36 -0300

    Agrego footer

--- gannis/src/App.js
+++ gannis/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import MainContent from './components/MainContent'; // Asegúrate de que la ruta sea correcta
+import Footer from './components/layouts/footer';
 
 const App = () => {
     const [darkMode, setDarkMode] = useState(false);
@@ -15,6 +16,7 @@ const App = () => {
         <BrowserRouter>
             <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
             <MainContent />
+            <Footer/>
         </BrowserRouter>
     );
 };
